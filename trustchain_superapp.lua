6817ef701932063905335d7a0ddc04941d58949c
T. H <caci802@gmail.com>
2024-03-08 11:54:23 +0100

    Almost perfect

--- trustchain_superapp.lua
+++ trustchain_superapp.lua
@@ -20,6 +20,36 @@ function Utils:parseIPv4(buffer, pinfo, tree, gloablOffset)
     return offset, { p1 = p1, p2 = p2, p3 = p3, p4 = p4, port = port, buffer = buffer(gloablOffset, offset - gloablOffset) }
 end
 
+function Utils:parseULong(buffer, pingo, tree, globalOffset)
+    local offset = globalOffset
+
+    local long = buffer(offset, 8)
+    offset = offset + 8
+
+    return offset, long
+end
+
+function Utils:parseUInt(buffer, pingo, tree, globalOffset)
+    local offset = globalOffset
+
+    local long = buffer(offset, 4)
+    offset = offset + 4
+
+    return offset, long
+end
+
+function Utils:parseString(buffer, pinfo, tree, globalOffset)
+    local offset = globalOffset
+
+    local size = buffer(offset, 1):uint()
+    offset = offset + 1
+
+    local string = buffer(offset, size)
+    offset = offset + size
+
+    return offset, string
+end
+
 
 -- IPv8 Protocol
 local IPv8 = {
@@ -71,8 +101,8 @@ function IPv8:parseSignature(buffer, pinfo, tree)
         return buffer, nil
     end
 
-    local signature = buffer(buffer:len() - 65, 64)
-    local remainder = buffer(0, buffer:len() - 65)
+    local signature = buffer(buffer:len() - 64, 64)
+    local remainder = buffer(0, buffer:len() - 64)
 
     return remainder, signature
 end
@@ -89,7 +119,7 @@ function IPv8:parseAuthenticationPayload(buffer, pinfo, tree)
         local subtree = tree:add(buffer(0, offset), "Authentication payload: " .. "(Unsigned)")
         subtree:add(len, "Length: " .. len:uint())
 
-        return offset, subtree
+        return buffer(offset), subtree
     end
 
     local public_key = buffer(offset, len:uint())
@@ -115,7 +145,7 @@ function IPv8:parseIPv8(buffer, pinfo, tree)
     buffer = buffer(1)
 
     -- Parse signnature if authenticated
-    if IPv8.Messages[msgID] ~= nil and IPv8.Messages[msgID].auth == true then
+    if IPv8.Messages[msgID:uint()] ~= nil and IPv8.Messages[msgID:uint()].auth == true then
         buffer, signature = IPv8:parseAuthenticationPayload(buffer, pinfo, tree)
     end
 
@@ -125,11 +155,186 @@ function IPv8:parseIPv8(buffer, pinfo, tree)
     return buffer, msgID
 end
 
+local function parseEVAWriteRequest(buffer, pinfo, tree)
+    local offset = 0
+    
+    local offset, Info = Utils:parseString(buffer, pinfo, tree, offset)
+    tree:add(Info, "Info: " .. Info:string())
+    
+    local offset, ID = Utils:parseString(buffer, pinfo, tree, offset)
+    tree:add(ID, "ID: " .. ID:string())
+    
+    local offset, nonce = Utils:parseULong(buffer, pinfo, tree, offset)
+    tree:add(nonce, "Nonce: " .. nonce:uint64())
+    
+    local offset, dataSize = Utils:parseULong(buffer, pinfo, tree, offset)
+    tree:add(dataSize, "Data Size: " .. dataSize:uint64())
+
+    local offset, blockCount = Utils:parseUInt(buffer, pinfo, tree, offset)
+    tree:add(blockCount, "Block Count: " .. blockCount:uint64())
+
+    local offset, blockSize = Utils:parseUInt(buffer, pinfo, tree, offset)
+    tree:add(blockSize, "Block Size: " .. blockSize:uint64())
+    
+    local offset, windowSize = Utils:parseUInt(buffer, pinfo, tree, offset)
+    tree:add(windowSize, "Window Size: " .. windowSize:uint64())
+
+    return buffer(0, offset)
+end
+
+IPv8.Messages[130] = {
+    id = 130,
+    app = "",
+    name = "EWA Write Request",
+    parse = parseEVAWriteRequest,
+    auth = true
+}
+
+local function parseEVAAcknowledgement(buffer, pinfo, tree)
+    local offset = 0
+    
+    local offset, nonce = Utils:parseULong(buffer, pinfo, tree, offset)
+    tree:add(nonce, "Nonce: " .. nonce:uint64())
+
+    local offset, ackWindow = Utils:parseUInt(buffer, pinfo, tree, offset)
+    tree:add(ackWindow, "ACK Window: " .. ackWindow:uint())
+
+    local unAckedBlocks = buffer(offset)
+    offset = offset + unAckedBlocks:len()
+    tree:add(unAckedBlocks, "UnACKed Blocks: " .. unAckedBlocks:raw())
+
+    return buffer(0, offset)
+end
+
+IPv8.Messages[131] = {
+    id = 131,
+    app = "",
+    name = "EVA Acknowledgement",
+    parse = parseEVAAcknowledgement,
+    auth = true
+}
+
+local function parseEVADataPayload(buffer, pinfo, tree)
+    local offset = 0
+
+    local offset, blockNumber = Utils:parseUInt(buffer, pinfo, tree, offset)
+    tree:add(blockNumber, "Block Number: " .. blockNumber:uint())
+    
+    local offset, nonce = Utils:parseULong(buffer, pinfo, tree, offset)
+    tree:add(nonce, "Nonce: " .. nonce:uint64())
+
+    local data = buffer(offset)
+    offset = offset + data:len()
+    tree:add(data, "Data: " .. data:raw())
+
+    return buffer(0, offset)
+end
+
+IPv8.Messages[132] = {
+    id = 132,
+    app = "",
+    name = "EVA Data",
+    parse = parseEVADataPayload,
+    auth = true
+}
+
+local function parseEVAError(buffer, pinfo, tree)
+    local offset = 0
+
+    local offset, info = Utils:parseString(buffer, pinfo, tree, offset)
+    tree:add(info, "Block Number: " .. info:string())
+    
+    local offset, message = Utils:parseString(buffer, pinfo, tree, offset)
+    tree:add(message, "Nonce: " .. message:string())
+
+    return buffer(0, offset)
+end
+
+IPv8.Messages[133] = {
+    id = 133,
+    app = "",
+    name = "EVA Error",
+    parse = parseEVAError,
+    auth = true
+}
+
+
+local function parseIntroductionResponse(buffer, pinfo, tree)
+    local offset = 0
+
+    local offset, IP = Utils:parseIPv4(buffer, pinfo, tree, offset)
+    tree:add(IP.buffer, "Destination Address: " .. IP.p1 .. "." .. IP.p2 .. "." .. IP.p3 .. "." .. IP.p4 .. ":" .. IP.port)
+    local offset, IP = Utils:parseIPv4(buffer, pinfo, tree, offset)
+    tree:add(IP.buffer, "Source LAN Address: " .. IP.p1 .. "." .. IP.p2 .. "." .. IP.p3 .. "." .. IP.p4 .. ":" .. IP.port)
+    local offset, IP = Utils:parseIPv4(buffer, pinfo, tree, offset)
+    tree:add(IP.buffer, "Source WAN Address: " .. IP.p1 .. "." .. IP.p2 .. "." .. IP.p3 .. "." .. IP.p4 .. ":" .. IP.port)
+    local offset, IP = Utils:parseIPv4(buffer, pinfo, tree, offset)
+    tree:add(IP.buffer, "LAN Introduction Address: " .. IP.p1 .. "." .. IP.p2 .. "." .. IP.p3 .. "." .. IP.p4 .. ":" .. IP.port)
+    local offset, IP = Utils:parseIPv4(buffer, pinfo, tree, offset)
+    tree:add(IP.buffer, "WAN Introduction Address: " .. IP.p1 .. "." .. IP.p2 .. "." .. IP.p3 .. "." .. IP.p4 .. ":" .. IP.port)
+    
+    local connectionType = buffer(offset, 1)
+    tree:add(connectionType, "Connection Type: " .. connectionType:uint())
+    offset = offset + 1
+
+    -- Identifier
+    local identifier = buffer(offset, 2)
+    tree:add(identifier, "Identifier: " .. identifier:uint())
+    offset = offset + 2
+    
+    -- Extra Bytes?
+    if buffer:len() > offset then
+        local extraBytes = buffer(offset)
+        tree:add(extraBytes, "Extra Bytes: " .. extraBytes:string())
+        offset = offset + extraBytes:len()
+    end
+    
+    return buffer(0, offset)
+end
+
 IPv8.Messages[245] = {
     id = 245,
     app = "",
     name = "Introduction Response",
-    parse = nil,
+    parse = parseIntroductionResponse,
+    auth = true
+}
+
+local function parseIntroductionRequest(buffer, pinfo, tree)
+    local offset = 0
+
+    local offset, IP = Utils:parseIPv4(buffer, pinfo, tree, offset)
+    tree:add(IP.buffer, "Destination Address: " .. IP.p1 .. "." .. IP.p2 .. "." .. IP.p3 .. "." .. IP.p4 .. ":" .. IP.port)
+    local offset, IP = Utils:parseIPv4(buffer, pinfo, tree, offset)
+    tree:add(IP.buffer, "Source LAN Address: " .. IP.p1 .. "." .. IP.p2 .. "." .. IP.p3 .. "." .. IP.p4 .. ":" .. IP.port)
+    local offset, IP = Utils:parseIPv4(buffer, pinfo, tree, offset)
+    tree:add(IP.buffer, "Source WAN Address: " .. IP.p1 .. "." .. IP.p2 .. "." .. IP.p3 .. "." .. IP.p4 .. ":" .. IP.port)
+
+    local connectionType = buffer(offset, 1)
+    tree:add(connectionType, "Connection Type: " .. connectionType:uint())
+    offset = offset + 1
+
+    -- Identifier
+    local identifier = buffer(offset, 2)
+    tree:add(identifier, "Identifier: " .. identifier:uint())
+    offset = offset + 2
+    
+    -- Extra Bytes?
+    if buffer:len() > offset then
+        local extraBytes = buffer(offset)
+        tree:add(extraBytes, "Extra Bytes: " .. extraBytes:string())
+        offset = offset + extraBytes:len()
+    end
+    
+    return buffer(0, offset)
+end
+
+
+IPv8.Messages[246] = {
+    id = 246,
+    app = "",
+    name = "Introduction Request",
+    parse = parseIntroductionRequest,
     auth = true
 }
 
@@ -193,39 +398,33 @@ IPv8.Messages[251] = {
     auth = false
 }
 
-
-local function parseIntroductionRequest(buffer, pinfo, tree)
+local function parseOpenPortPayload(buffer, pinfo, tree)
     local offset = 0
 
-    local offset, IP = Utils:parseIPv4(buffer, pinfo, tree, offset)
-    tree:add(IP.buffer, "Destination Address: " .. IP.p1 .. "." .. IP.p2 .. "." .. IP.p3 .. "." .. IP.p4 .. ":" .. IP.port)
-    local offset, IP = Utils:parseIPv4(buffer, pinfo, tree, offset)
-    tree:add(IP.buffer, "Source LAN Address: " .. IP.p1 .. "." .. IP.p2 .. "." .. IP.p3 .. "." .. IP.p4 .. ":" .. IP.port)
-    local offset, IP = Utils:parseIPv4(buffer, pinfo, tree, offset)
-    tree:add(IP.buffer, "Source WAN Address: " .. IP.p1 .. "." .. IP.p2 .. "." .. IP.p3 .. "." .. IP.p4 .. ":" .. IP.port)
-
-    local connectionType = buffer(offset, 1)
-    tree:add(connectionType, "Connection Type: " .. connectionType:uint())
-    offset = offset + 1
-
-    -- Identifier
-    local identifier = buffer(offset, 2)
-    tree:add(identifier, "Identifier: " .. identifier:uint())
+    local portToOpen = buffer(0,2)
     offset = offset + 2
-    
-    local extraBytes = buffer(offset)
-    offset = offset + extraBytes:len()
-    
+    tree:add(portToOpen, "Port to open: " .. portToOpen:uint())
+
     return buffer(0, offset)
 end
 
-IPv8.Messages[246] = {
-    id = 246,
+IPv8.Messages[252] = {
+    id = 251,
     app = "",
-    name = "Introduction Request",
-    parse = parseIntroductionRequest,
-    auth = true
+    name = "Port Open Request",
+    parse = parseOpenPortPayload,
+    auth = false
 }
+IPv8.Messages[253] = {
+    id = 251,
+    app = "",
+    name = "Port Open Response",
+    parse = parseOpenPortPayload,
+    auth = false
+}
+
+
+
 
 -- create a function to dissect it
 function superapp_proto.dissector(buffer, pinfo, tree)
